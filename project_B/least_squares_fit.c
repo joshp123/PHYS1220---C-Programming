1ba6f1398b809879a7c878bf64fe3ecd3952b2df
Josh Palmer <joshpalmer123@gmail.com>
2012-03-05 08:21:34 +0000

    version that actually compiles (it's still pretty broken though. errors on file-read portion, isn't scanning in data)

--- project_B/least_squares_fit.c
+++ project_B/least_squares_fit.c
@@ -4,42 +4,40 @@ Project B: Parameters of a least squares fit
 
 Takes data from a file / user input and calculates a least squares fit of them, returns equation in the form y = mx + c
 
+this terrible source code available free for you to do whatever the hell with at http://github.com/joshp123 (idk why you would but whatever it was useful for version control)
+
 */
 
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
-/* Structure for the final result. returns m and c values and errors on both, i.e. y = mx + c */
-typedef
- struct {
+// Structure for the final result. returns m and c values and errors on both, i.e. y = mx + c 
+typedef struct {
 	float m;
 	float merr;
 	float c;
 	float cerr;
 }	graph;
 
-/*structure for the input data - length, arrays of x, y, and y errors */
+// Structure for the input data - length, arrays of x, y, and y errors 
 
 typedef struct{
 	int length;
-	double x[1000]; // todo: figure out if you can dynamically extend the arrays if you want to be retarded and use a billion data points or whatever instead of some arbitrary limit
+	double x[1000]; // TODO: figure out if you can dynamically extend the arrays if you want to be retarded and use a billion data points or whatever instead of some arbitrary limit
 	double y[1000];
 	double yerr[1000];
 } dataset;
 
-/*Functions to flush to end of line - if we want to ignore "," between values in .csvs we'll probably need to modify this at some point
-*/
+// Functions to flush to end of line - this is useful for a reason. i'm not entirely sure what that reason is but whatever 
 
-void flushf(FILE *getfile){  // complier only plays nice when you use void instead of ()  vOv
+void flushf(FILE *getfile){  
 	while(fgetc(getfile)!='\n'){}
 }
 
-void flush(void){  // complier only plays nice when you use void instead of ()  vOv
+void flush(){  // LCC complier only plays nice when you use flush(void) instead of flush()  . GCC on the other hand does not. welp that's my cool compiler story for today
 	while(getchar()!='\n'){}
 	}
-
-
-
 
 
 /*
@@ -59,93 +57,94 @@ int UserSelection(int options){
 	return input;
 }
 
-/*this get input from a .csv or .txt file formatted in the form:
+int GetFileLineCount(FILE *FILE){
+	int linecount;
+	// TODO: finish this (iterate over entire file reading \n, and incrementing, break at EOF)
+
+	// TODO: since this scans for \n might be possible to do non-numeric data checking here? idk
+	return linecount;
+}
+
+/*
+Function to read 3 columns of data in from a file.
+
+Gets input from a .csv or .txt file formatted in the form:
 x1, y1, yerr1
 x2, y2, yerr2
 x3, y3, yerr3
 
 etc.
 
-This is smart because it's how excel saves .csv files and excel is nice and easy and yeah you get the idea.	!
+(This is how excel saves stuff in the .csv format, which is why this is actually useful.)
+
+It returns data in the struct "output" as described above.
 
 TODO: make it so you can call this exe from the commandline being all leastsquares.exe -somefilename.whatever and it opens that. It might even work if you do leastsquares.exe -"C:\gbs\555\foo.bar" but i'll get to that when i do this (see you in 2015). I suppose you could also sorta prompt for it in the program but that's kinda stupid, it would be more sensible to make a GUI and use window's browse thing but now i'm getting carried away
 
+omg i acutally did this fuck yeah i'm awesome :sun:
+
 */
 
-// NB I SWITCHED FROM COLUMS TO ROWS BECAUSE THAT MAKES MORE SENSE, GO CHECK EVERYTHING IS RIGHT YOU FAGGOT
-
-dataset GetInputNew(int type){
+dataset GetInput(char *filename, int verbose){ // either char * or char *filename idk
 	printf("Commencing file open routine\n");//delme
 	FILE *fin;
-	int i = 0; // i = columns, i.e. # of datapoints
-	int a,b,c;
-	int j = 0; // j = rows, i.e. # of series. here should never exceed 2 (x has j=0, y has j = 1, yerr has j = 2)
-	// int xcount, ycount, yerrcount, - add this line later to verify same number of points on each line
-	double data[2][256];
+	int i;
+	// TODO int xcount, ycount, yerrcount, - add this line later to verify same number of points on each line. necessarry? y/n
 	dataset output;
-	if(type == 1){
-		fin = fopen("data.txt", "r");
-		printf("Opening data.txt file\n");//delme
+
+	if(verbose == 1){
+		printf("Opening %s\n",filename);// diagnostic line
 	}
-	else {
-		fin = fopen("data.csv", "r");
-		printf("Opening data.csv file\n"); //delme
+	
+	fin = fopen(filename, "r");
+
+	if(fin==0 && verbose == 1) {
+		printf("Could not open file.\n");
+		return;
 	}
-	if(fin==0) {
-		printf("Could not open file.	\n");
-		// return;
-	}
-	while((fgetc(fin) != 'EOF') || j <= 2){ // stops at EOF and when j<=2. TODO: add an alert when j > 2 so you can tell the user that they are dumb and should RTFM
-		if(fgetc(fin) != '\n'){
-			if(fgetc(fin) != ','){
-				data[j][i] = fgetc(fin); // todo: check that this is actually a float
-				printf("%f \n",data[j][i]);
+
+	// file open routine complete, get linecount
+
+	output.length = 10; // TEST LINE PLZ REMOVE
+
+	//output.length = GetFileLineCount(fin);
+
+	// number of data points counted, now to read in the actual data
+
+	while(fgetc(fin) != 'EOF'){ // terminates when it hits EOF
+		for (i = 0; i < output.length; ++i){
+			int valid = fscanf(fin, "%f,%f,%f", &output.x[i], &output.y[i], &output.yerr[i]);
+			printf("x = %.2f\ty = %.2f\ty error = %.2f\n",output.x[i],output.y[i],output.yerr[i]); // diagnostic line
+			if(valid !=0){
+				printf("Non-numeric data detected in file.\n");
 			}
-			elsei
-				{++;
-				s++; //use s as a total number of values
+			if (output.yerr[i] == 0){
+				if (verbose == 1){
+					printf("Your data contains errors that are equal to zero. This is invalid. Program exiting\n");
+				}
 			}
-		}
-		else{
-			j++;
-			i=0;
+			return;
 		}
 	}
 
 	fclose(fin);
 
-	// start processing data after here, adding it into arrays etc etc etc
-
-	if (j > 2)	{
-		printf("More than 3 data-sets detected. Ignoring all data after the first 3 sets.\n"); // should probably stop it adding it to the temporary array if you have 5k rows or whatever just for the sake of making it a bit faster but whatever i'm lazy and it doesn't really matter
-	}
-	output.length = c/j;
-	printf("Your dataset has %i constituent points\n",output.length);
-	for(b = 0; b <= j; b++){
-		for(a = 0; a <= i; a++){
-			printf("Row %i Column %i : %f\n",b,g,data[g][b]); // disgnostic prints itt TODO: make this sexy by adding a bunch of tabs and shit and making it wrap etc
-
-			switch(b) {  // based on the column it's currently iterating on, picks which output set to do. This is necessary because doing the sumsquares shit on an array of arrays would be kinda gay whereas it's easier if they're 3 separate arrays.
-				case 0:
-					output.x[a] = data[a][g];
-					break;
-				case 1:
-					output.y[a] = data[a][g];
-					break;
-				case 2:
-					output.yerr[a] = data[a][g];
-					break;
-	}
+	if (verbose = 1){
+		printf("Your dataset has %i constituent points:\n\n",output.length);
+		for (i = 0; i < output.length; ++i)
+		{
+			printf("x = %.2f\ty = %.2f\ty error = %.2f\n",output.x[i],output.y[i],output.yerr[i]);
 		}
 	}
+	
 	return output;
 }
 
 /*
-Function to calculate p, q, r, s, t and obtain m and c from them
+Function to calculate the actual least squares parameters given a set of data
 */
 
-graph LeastSquares(double x[], double y[], double yerr[], int length){
+graph LeastSquares(int length, double x[], double y[], double yerr[]){
 	float p,q,r,s,t,delta,m; // p,q,r,s,t,delta as given in formulae; m = 1/e^2
 	int i;
 	graph result;
@@ -172,14 +171,49 @@ graph LeastSquares(double x[], double y[], double yerr[], int length){
 	return result;
 }
 
-int main(){
+int main(int argc, char *argv[]){
+	char filename[256];
+	int verbose = 0;
+	// process command line args
+	for (int i = 0; i < argc; ++i)
+	{
+		printf("%s\n",argv[i]); // diagnostic line
+	}
+	if ((argc > 1 && strcmp(argv[2],"-help") == 0) || argc == 1){  // display help message if either no arguments passed or "-help" passed
+		printf("\n\nThis program calculates the least squares fit of a set of data. Supported file format is .csv. Currently only supports comma as delimiter.\n\nDefault usage is: leastsquares.exe \"filename\" [-v]\n\n\t-help \tDisplays this help message\n\t-v\tVerbose mode: displays errors/status, prints the\n\t\tdata that it reads for verification purposes.\n\n");
+
+		// delete this later:
+		printf("LAZY MODE TO CHECK THAT MY THIS ACTUALLY WORKS: hit 1 to continue, 2 to gtfo\n");
+		int cont = UserSelection(2);
+		if (cont == 1){
+		}
+		else {
+			return 0; // leave this in (i.e. exit after help message)
+		}
+	}
+
+	else if(argc > 1){
+		printf("accepting filename\n"); // diagnostic line
+		strcpy(filename,argv[1]);
+		if (strcmp(argv[2],"-v") == 0){
+			printf("verbosity enabled\n"); // diagnostic line
+			verbose = 1;
+
+		}
+	}
 	int choice;
+	printf("verbosity = %d \n",verbose);
+	// command line args processed, now go ~do shit~
+
 	dataset input;
 	graph output;
-	printf("Is your data in a .txt or a .csv file? Enter 1 for txt, 2 for csv.\n"); // NB .CSV DOESN'T WORK YET. LOL EFFORT
-	choice = UserSelection(2); // do userselection function, ensures user only picks 1 or 2
-	input = GetInputNew(choice); // go open the file
-	output = LeastSquares(input.x,input.y,input.yerr,input.length);
-	printf("The least squares fit of this data is: y = (%2f \361 %2f)x + (%2f \361 %2f) \n",output.m,output.merr,output.c,output.cerr);
+	// deprecated:
+	// printf("This program calculates the least squares fit of a set of data.\nYour data should be in the same directory as this executable,  \n");
+	// printf("Is your data in a .txt or a .csv file? Enter 1 for txt, 2 for csv.\n");
+	// choice = UserSelection(2); // do userselection function, ensures user only picks 1 or 2
+
+	input = GetInput(filename,verbose); // go open the file
+	output = LeastSquares(input.length,input.x,input.y,input.yerr);
+	printf("The least squares fit of this data is: y = (%.2f \361 %.2f)x + (%.2f \361 %.2f) \n",output.m,output.merr,output.c,output.cerr);
 	return 0;
 }
